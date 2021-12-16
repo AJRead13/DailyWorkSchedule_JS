d2c1d861e5ff363ac1a2c7768f37936954f98875
Andrew Read <aj.read13@gmail.com>
2021-12-16 18:23:47 -0500

    working localstorage

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -73,13 +73,11 @@ function renderTasks() {
       return;
    }
 }
-
-function saveTask() {
-   var savedTask = taskInput.value;
-   var taskedTime = hour.text;
-   localStorage.setItem()
-
-}
+function saveTask(event) {
+   event.preventDefault;
+   var hour = $(this).parent.parent.child[0].val()
+   
+}   
 // console.log(taskInput.val);
 
 // function writeTask() {
@@ -100,83 +98,3 @@ function saveTask() {
 
 
 //saveBtn.addEventListener("click", addTask)
-
-
-// varparentcontainertr=this.captureEvents.captureEventsvarhour=parentcontainertr.attrid.splimport {  } from "varinputcontrol - parentcontainertr.findinput
-// varvalue-inputcontrol.varvalueclocalroage.setitemhourvalue";
-//Saving the task in the localstorage and displaying it
-// saveBtn.on("click", function () {
-//    var timeSlot = $(this).parent().parent()
-//    var hour = timeSlot.attr("id").split("-")[1]
-//    var userInput = timeSlot.find("input")
-//    var value = userInput.val()
-//    localStorage.setItem(hour, value)
-// })
-// for (var index = 9; index < 18; index++) {
-//    var parentContainerTr = $("#hour-" + index)
-//    var userInput = parentContainerTr.find("input")
-//    var value = localStorage.getItem(index)
-//    userInput.val(value)
-// }
-//highlighting hours past/present/future
-//set time format to military time
-
-// function illuminate()   {
-//    var currentHour = moment()
-//    $(".time").each(function(){
-//       var taskRow = $(this)
-//       var tableHeader = taskRow.find('th')
-//       var tableData = taskRow.attr("id").split("-")[1]
-//       if (hour < currentHour) {
-//          tableHeader.addClass("past")
-//          tableData.addClass("past")
-//       }
-//       else if (hour === currentHour)
-
-//    })
-// }
-
-// function illuminate() {
-   
-// }
-
-// function illuminate() {
-//    var currentHour = moment()
-//    $(".time").each(function() {
-//       var trContainer = $(this)
-//       var th = trContainer.find("th")
-//       var td = trContainer.attr("id").split("-")[1]
-//       if (hour < currentHour) {
-//          th.addClass("past")
-//          td.addClass('past')
-//       }
-//       else if (hour === currentHour) {
-//          th.addClass("present")
-//          td.addClass("present")
-//       }
-//       else if (hour > currentHour) {
-//          th.addClass("future")
-//          td.addClass("future")
-//       }
-//    })
-// }
-
-
-// var currentHour = moment();
-// $(".btn".on*"click"", fucntion*(){
-//    var parentcontairtr =$)this).parent().
-// })
-
-// .time.eachfunction vartrcontainer-this
-// varth-trcontainer.findth
-// vartd-trcontainer.findtd
-// varhour-trcontainer.attrid.split-1
-// ifhour<currenthour
-// th.addclass-past
-// th.addclass-past
-// elseifhour==currenthour
-// th.addclasspresent
-// td.addclassprestn
-// elseth.addclassfuture
-// tdaddclassfuture
-// }
