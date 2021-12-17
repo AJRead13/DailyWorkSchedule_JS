d22af33dd5e4e157f9b176c294ca02cdb9cdc51e
Andrew Read <aj.read13@gmail.com>
2021-12-16 23:24:48 -0500

    messed with it a lot, colors are working, local storage is shoddy at best

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,18 +6,15 @@ var dateTime = function () {
 setInterval(dateTime, 1000);
 var taskBlock = $(".writtenTask");
 var saveBtn = $(".saveBtn");
-var hour = $(".hour");
+//var hour = $(".hour");
 var tableHeader = $("th");
 var tableData = $('td');
+var timeRow = $("tr");
 //var dayHour = hour;
 var taskInput = $(".taskInput")
-var hourlyTask = {
-taskHour: hour.text,
-theTask: taskInput.value,
-}
-//console.log(dayHour);
+
 var saveBtn = $(".saveBtn")
-var taskInput =$(".taskInput")
+var taskInput = $(".taskInput")
 var nine = document.getElementById("time-9").getAttribute("data-time");
 var ten = document.getElementById("time-10").getAttribute("data-time");
 var eleven = document.getElementById("time-11").getAttribute("data-time");
@@ -27,66 +24,147 @@ var fourteen = document.getElementById("time-14").getAttribute("data-time");
 var fifteen = document.getElementById("time-15").getAttribute("data-time");
 var sixteen = document.getElementById("time-16").getAttribute("data-time");
 var seventeen = document.getElementById("time-17").getAttribute("data-time");
+var timeData = $("tr[data-time]")
+//console.log(timeData);
 
 var workHours = document.querySelectorAll(".timeZone");
 //console.log(workHours);
 console.log(workHours[0].getAttribute("data-time"));
 
 var scheduleHour = []
-console.log(scheduleHour);
 var globalTime = moment().format("HH");
 console.log(globalTime);
 var timeNumber = parseInt(globalTime);
 console.log(timeNumber);
 
 function illuminate() {
-   for(i = 9; i < 18; i++){
-   var newHour = parseInt(i);
-   scheduleHour.push(i);
-   console.log(scheduleHour);
-   if (newHour < timeNumber){
-      tableHeader.addClass("past");
-      tableData.addClass("past");
-   }
-   else if (newHour === timeNumber){
-      tableHeader.addClass("present");
-      tableData.addClass("present");
-   }
-   else if (newHour > timeNumber){
-      tableHeader.addClass("future");
-      tableData.addClass("future");
-   }
-}
+   timeRow.each(function () {
+      var newHour = $(this);
+      var hourEl = newHour.children(".hour").text();
+      hourEl = parseInt(hourEl);
+      if (timeNumber > hourEl) {
+         newHour.find("th").addClass("past");
+         newHour.find("td").addClass("past");
+      }
+      if (timeNumber === hourEl) {
+         newHour.find("th").addClass("present");
+         newHour.find("td").addClass("present");
+      }
+      if (timeNumber < hourEl) {
+         newHour.find("th").addClass("future");
+         newHour.find("td").addClass("future");
+      }
+   })
 }
 illuminate();
 
 var hourlyTask = {
-   taskHour: hour.text,
-   theTask: taskInput.value,
+   taskHour: "hour",
+   theTask: "task",
+}
+// console.log(hourlyTask);
+
+// function renderTasks() {
+//    if (task === )
+//       var task = localStorage.getItem(hour);
+//    console.log(task);
+// }
+// renderTasks();
+
+// for (let i = 9; i < 18; i++) {
+//    console.log(timeData);
+//    var numberTime = timeData.split("=")[1]
+// }console.log(numberTime);
+function saveTask(event) {
+   //writeTask.each(function(){
+   event.preventDefault();
+   var hour = $(event.target).parent().siblings(".hour").text();
+   hour = parseInt(hour);
+   var task = $(event.target).parent().siblings(".tasks").children(".taskInput").val();
+   //var taskArray = JSON.parse(localStorage.getItem("hourlyTask") || "[]");
+   var hourlyTask = {
+      taskHour: hour,
+      theTask: task,
    }
-console.log(hourlyTask);
+   taskArray.push(hourlyTask),
+      localStorage.setItem("taskArray", JSON.stringify(taskArray));
+
+}
+
+saveBtn.on("click", saveTask);
+
+// var hourRow = hourSlot.attr("id").split("-")[1];
+// console.log(hourRow);
+
+
+var writeTask = $("input");
+var taskArray = JSON.parse(localStorage.getItem("hourlyTask") || "[]");
+
 
 function renderTasks() {
-   var task = localStorage.getItem(theTask);
-   var taskTime = localStorage.getItem(taskHour);
-   if (!task || !taskTime) {
-      return;
-   }
-}
-function saveTask(event) {
-   event.preventDefault;
-   var hour = $(this).parent.parent.child[0].val()
-   
-}   
-// console.log(taskInput.val);
+   var dataArray = localStorage.getItem("taskArray");
+   console.log(dataArray);
+   var dataObject = JSON.parse(dataArray);
+   var hourSlot = $("tbody");
+   console.log(hourSlot.children().attr("id").split("-")[1]);
 
-// function writeTask() {
-//    if (taskInput.value){
-//    var writtenTask = document.createElement("p");
-//    writtenTask.textContent = taskInput.val;
-//    taskInput.append(writtenTask);  
+   for (i = 0; i < dataObject.length; i++) {
+      dataObject.sort(function (a, b) { return a.taskHour - b.taskHour });
+      var dataHour = dataObject[i].taskHour;
+      console.log(dataHour);
+      var dataTask = dataObject[i].theTask;
+      console.log(dataTask);
+      if (dataHour === hourSlot.children().attr("id").split("-")[1]){
+         var timeAgain = hourSlot.children("#time-" + dataHour).children(".tasks").children();
+         timeAgain.val(dataTask);
+         console.log(timeAgain);
+         // taskInput.val(dataTask);
+      }
+      else{
+         return;
+      }
+
+
+
+      // hourRow.val(dataTask);
+      // taskInput.val(dataTask);
+      // console.log(dataObject);
+      // console.log(dataObject);
+      // console.log(dataObject[i].theTask);
+      // console.log(dataObject[i].taskHour);
+   } // var dataArray = localStorage.getItem("taskArray");
+   // console.log(dataArray);
+   // var dataObject = JSON.parse(dataArray);
+
+
+}
+renderTasks();
+
+// function saveTask(event) {
+//    //writeTask.each(function(){
+//    event.preventDefault();
+//    var hour = $(event.target).parent().siblings(".hour").text();
+//    hour = parseInt(hour);
+//    var task = $(event.target).parent().siblings(".tasks").children(".taskInput").val();
+//    //var taskArray = JSON.parse(localStorage.getItem("hourlyTask") || "[]");
+//    var hourlyTask = {
+//       taskHour: hour,
+//       theTask: task,
 //    }
-//    else return;   
+//    taskArray.push(hourlyTask),
+//       localStorage.setItem("taskArray", JSON.stringify(taskArray));
+
+// }
+
+// saveBtn.on("click", saveTask);
+
+// // function writeTask() {
+// //    if (taskInput.value){
+// //    var writtenTask = document.createElement("p");
+// //    writtenTask.textContent = taskInput.val;
+// //    taskInput.append(writtenTask);
+// //    }
+// //    else return;
 // }
 // saveBtn.on("click", writeTask);
 // function addTask(event) {
